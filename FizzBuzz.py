06384f359b4c7a1570ae08536f6a77ab0118a9a5
Denis <deniss777@live.ru>
2022-01-25 18:01:04 +0200

    Initial commit

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return  'FizzBuzz'
+    elif number % 3 == 0:
+        return 'Fizz'
+    elif number % 5 == 5:
+        return 'Buzz'
+    else:
+        return number
+for number in range(1, 101):
+        print(fizzbuzz(number))
+
+
+
